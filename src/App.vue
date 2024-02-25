187acadac62430f19075f211247b2f759371efe3
Stekl0 <146914507+GreyStekl0@users.noreply.github.com>
2024-02-25 16:56:33 +0500

    A sketchbook block has been made

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import VPaper from "@/components/VPaper.vue";
 import VWhiteness from "@/components/VWhiteness.vue";
 import VItems from "@/components/VItems.vue";
 import VPartners from "@/components/VPartners.vue";
+import VSketchbook from "@/components/VSketchbook.vue";
 </script>
 
 <template>
@@ -13,6 +14,7 @@ import VPartners from "@/components/VPartners.vue";
     <v-whiteness />
     <v-items />
     <v-partners />
+    <v-sketchbook />
   </main>
 </template>
 
---
+++ src/components/VSketchbook.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="sketchbook">
+    <div class="container">
+      <div class="row">
+        <div class="sketchbook__card col-4">
+          <h2>Simple скетчбук</h2>
+          <p>
+            80 листов, твердая обложка, бумага  Fabriano 200 г/м2.
+            Подойдет и для графики и для акварели.
+            Для самых смелых творческих замыслов!
+          </p>
+          <button>Купить</button>
+        </div>
+        <div class="sketchbook__img col-8">
+          <img src="/sketchbook.png" alt="sketchbook">
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
