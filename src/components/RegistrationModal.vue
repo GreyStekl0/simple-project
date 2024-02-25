574aa55d178ffad290f29dc77eb080d9abe62480
Stekl0 <146914507+GreyStekl0@users.noreply.github.com>
2024-02-25 21:39:41 +0500

    registration form has been made

---
+++ src/components/RegistrationModal.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+const props = defineProps(['show']);
+const emit = defineEmits(['close']);
+</script>
+
+<template>
+  <section class="registration" v-if="props.show">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-6">
+          <div class="registration__text">
+            <h2>Регистрация</h2>
+            <p>Зарегистрируйтесь, чтобы получить доступ к личному кабинету</p>
+          </div>
+        </div>
+        <div class="col-6">
+          <form class="form" method="post" @submit.prevent="onSubmit">
+            <label class="mb-3">
+              <input type="text"
+                     placeholder="Имя"
+                     title="Введите ваше имя"
+                     v-model="name" required>
+            </label>
+            <label class="mb-3">
+              <input type="email"
+                     placeholder="Электронная почта"
+                     title="Введите вашу электронную почту"
+                     v-model="email" required>
+            </label>
+            <label class="mb-3">
+              <input type="password"
+                     placeholder="Пароль"
+                     title="Введите ваш пароль"
+                     v-model="password" required>
+            </label>
+            <button type="submit">Зарегистрироваться</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+@import "src/assets/RegistrationModal.scss";
+</style>
--- src/components/VHeader.vue
+++ src/components/VHeader.vue
@@ -1,5 +1,16 @@
 <script setup>
+import { ref } from 'vue';
+import RegistrationModal from './RegistrationModal.vue';
 
+let showModal = ref(false);
+
+const toggleModal = () => {
+  showModal.value = !showModal.value;
+};
+
+const closeModal = () => {
+  showModal.value = false;
+};
 </script>
 
 <template>
@@ -9,15 +20,15 @@
         <div class="navigation__wrapp">
           <img src="/logo.svg" alt="logo">
           <ul class="navigation__list">
-            <li class="navigation__list-item">Продукция</li>
-            <li class="navigation__list-item">Материалы</li>
-            <li class="navigation__list-item">О нас</li>
-            <li class="navigation__list-item">Контакты</li>
+            <li class="navigation__list-item"><a href="#" class="navigation__list-link">Продукция</a></li>
+            <li class="navigation__list-item"><a href="#" class="navigation__list-link">Материалы</a></li>
+            <li class="navigation__list-item"><a href="#" class="navigation__list-link">О нас</a></li>
+            <li class="navigation__list-item"><a href="#" class="navigation__list-link">Контакты</a></li>
             <li class="navigation__list-box">
               <a href="#" class="navigation__list-box-search">
                 <img src="/Search.svg" alt="search">
               </a>
-              <a href="#" class="navigation__list-box-login">
+              <a href="#" class="navigation__list-box-login" @click.prevent="toggleModal">
                 <img src="/log_in.svg" alt="login">
               </a>
             </li>
@@ -26,6 +37,7 @@
       </div>
     </nav>
   </header>
+  <RegistrationModal :show="showModal" @close="closeModal" />
 </template>
 
 <style scoped>
